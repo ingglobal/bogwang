7e297b4f392a7b2d3fba355e456730a7eb099973
root <root@q381-1158.cafe24.com>
2021-11-11 19:48:26 +0900

    api working 1111 1948

---
+++ device/itm_label/form.php
@@ -0,0 +1,56 @@
+<?php
+include_once('./_common.php');
+// 인스타 로그인후 인스타쪽 피드 배열을 받아서 올스타에 업데이트하는 폼을 임시로 구현한 페이지입니다.
+// 실제로는 member_log.php가 크롤링 서버에서 정보를 받아서 저장합니다.
+// 파일명이 실제로는 member_feed_log.php가 더 적합할 수도..
+
+// $com_idx_array = array(9999,67,66,65,64,10000);
+$part_no_array = array('88700-J9110PUR','C89460-CG930SIT','89909-CG930SIT','89908-CG930SIT','89909-CG930MIX');
+$barcode_array = array('211018_00005_C89460-CG930SIT_19204DERH00011530','211018_00005_C89460-CG930SIT');
+$mms_idx_array = array(7,8,9,10);
+?>
+<style>
+	form {padding:10px 100px 100px;}
+	section {border:solid 1px #ddd;font-size:0.8em;}
+	section ul {margin-left:-11px;}
+	table {border:solid 1px #666;margin-top:10px;}
+	table caption {text-align:left;background:#222;color:white;padding:7px;}
+	tr td:first-child {width:100px;font-size:0.9em;}
+	tr td input {border:solid 1px #ddd;padding:4px;}
+    button {background:#37a7ff;padding:10px 110px;font-size:1.5em;border:none;border-radius:4px;cursor:pointer;}
+</style>
+
+<form id="form01" action="./form2.php">
+<h1>생산시작 API</h1>
+<section>
+	<ul>
+		<li>바코드 출력과 동시에 통신하는 API입니다.</li>
+		<li>g5_1_item 테이블에 새로운 record가 생성됩니다.(상태값=ing) / 관련자재(g5_1_meterial)들의 상태값들도 함께 변경됩니다.(상태값=ing) </li>
+		<li>재발행인 경우도 같은 값으로 던져주시면 됩니다.</li>
+		<li>반환(return)값: 자재재고 경고, itm_idx, itm_status</li>
+	</ul>
+</section>
+
+<table>
+	<tr><td>공통 토큰</td><td><input type="text" name="token" value="1099de5drf09"></td></tr>
+</table>
+
+<table>
+	<caption>생산시작</caption>
+	<!-- <tr><td>업체 idx1</td><td><input type="text" name="com_idx[]" value="8"></td></tr> -->
+	<tr><td>IMP idx1</td><td><input type="text" name="imp_idx[]" value="<?=rand(1,16)?>"></td></tr>
+	<tr><td>MMS idx1</td><td><input type="text" name="mms_idx[]" value="<?=$mms_idx_array[rand(0,sizeof($mms_idx_array)-1)]?>"></td></tr>
+	<tr><td>작업구간1</td><td><input type="text" name="shf_idx[]" value="<?=rand(1,8)?>"></td></tr>
+	<tr><td>파트넘버</td><td><input type="text" name="bom_part_no[]" value="<?=$part_no_array[rand(0,sizeof($part_no_array)-1)]?>"></td></tr>
+	<tr><td>바코드</td><td><input type="text" name="itm_barcode[]" value="<?=$barcode_array[rand(0,sizeof($barcode_array)-1)]?>" style="width:370px;"></td></tr>
+	<tr><td>위치</td><td><input type="text" name="trm_idx_location[]" value="<?=rand(1,4)?>"></td></tr>
+	<tr><td>날짜1</td><td><input type="text" name="itm_date[]" value="<?=date("y.m.d",time())?>"></td></tr>
+	<tr><td>시간1</td><td><input type="text" name="itm_time[]" value="<?=date("H:i:s",time()-rand(0,86400))?>"></td></tr>
+	<!-- <tr><td>메시지1</td><td><input type="text" name="itm_message[]" value="에러코드입니다."></td></tr> -->
+</table>
+
+<hr>
+<button type="submit">확인</button>
+</form>
+
+
